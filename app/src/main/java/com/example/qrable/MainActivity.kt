5e46d202ce6b1924815dcd92348d9193fdedcc1b
ShopOne <oneky8080@gmail.com>
2020-02-05 22:38:47 +0900

    use recycleView ..?

--- app/src/main/java/com/example/qrable/MainActivity.kt
+++ app/src/main/java/com/example/qrable/MainActivity.kt
@@ -5,19 +5,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list : MutableList<MutableMap<String,String>> = mutableListOf()
+        val item = mutableMapOf("name" to "you", "price" to "we")
+        list.add(item)
+        val layout = LinearLayoutManager(applicationContext)
+
+        val adapter = RecyclerListAdapter(list)
+        lvMenu.setHasFixedSize(true)
+        lvMenu.adapter = adapter
+        lvMenu.layoutManager = layout
     }
     private inner class RecyclerListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
     }
-    /*
-    private inner class RecyclerlistAdapter(private val _listdata: MutableList<MutableMap<String,Any>>)
+    private inner class RecyclerListAdapter(private val _listData: MutableList<MutableMap<String,String>>)
         : RecyclerView.Adapter<RecyclerListViewHolder>(){
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerListViewHolder {
@@ -26,8 +37,14 @@ class MainActivity : AppCompatActivity() {
             val view = inflater.inflate(R.layout.shop_cell,parent,false)
             return RecyclerListViewHolder(view)
         }
+
+        override fun onBindViewHolder(holder: RecyclerListViewHolder, position: Int) {
+        }
+
+        override fun getItemCount(): Int {
+            return _listData.size
+        }
     }
 
-     */
 
 }
